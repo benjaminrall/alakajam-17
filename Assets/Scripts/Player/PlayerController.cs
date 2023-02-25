ce3b9baa6ca62350d689e9321b65f1840edf9780
benjaminrall <benr2508@aol.com>
2023-02-25 19:08:41 +0000

    fixed weird physics bug

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -45,6 +45,8 @@ namespace Player
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody.inertiaTensor = Vector3.one;
+            _rigidbody.centerOfMass = Vector3.zero;
         }
 
         private void Update()
